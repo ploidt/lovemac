530866a1ebfaf4d624eccd67ed8a045cbb4b0472
ploidt <ploid@Ploid.local>
2014-12-02 14:50:38 +0700

    first commit

---
+++ lovemac.rb
@@ -0,0 +1,13 @@
+def lovemac(number)
+  # Put your code in here  
+  if (number%15==0)
+			puts "HateWindows"
+
+		elsif (number%3==0)
+			puts "Love"
+		elsif (number%5==0)
+			puts  "Mac"
+		else
+			puts number
+		end
+end
---
+++ lovemac_spec.rb
@@ -0,0 +1,9 @@
+require "./lovemac.rb"
+
+describe "lovemac" do
+  # Write your test cases in here
+  (1..50).each do |number|
+      	puts lovemac(number)
+      end
+
+end
